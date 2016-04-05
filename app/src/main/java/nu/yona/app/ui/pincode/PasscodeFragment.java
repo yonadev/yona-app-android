09801a5f606a4fe23720ccb0852bfee121fe36ea
Bhargav <bsuthar@mobiquityinc.com>
2016-04-05 15:48:48 +0530

    update touchable false on other field

--- app/src/main/java/nu/yona/app/ui/pincode/PasscodeFragment.java
+++ app/src/main/java/nu/yona/app/ui/pincode/PasscodeFragment.java
@@ -97,15 +97,10 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
 
 
         updateScreenUI();
+        resetDigit();
 
         return view;
 
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        resetDigit();
     }
 
     private void updateScreenUI() {
@@ -117,7 +112,8 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
                 populateVerifyPasscodeView();
                 visibleView();
             } else if (screen_type.equalsIgnoreCase(AppConstant.LOGGED_IN)) {
-                //Todo for loggin screen
+                visibleLoginView();
+                populateLoginView();
             } else if (screen_type.equalsIgnoreCase(AppConstant.SMS)) {
                 //Todo for SMS screen
             }
@@ -136,6 +132,20 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
         profile_progress.setVisibility(View.VISIBLE);
     }
 
+    private void visibleLoginView() {
+        passcode_title.setVisibility(View.VISIBLE);
+        passcode_description.setVisibility(View.GONE);
+        profile_progress.setVisibility(View.GONE);
+        passcode_reset.setVisibility(View.VISIBLE);
+    }
+
+    private void populateLoginView() {
+        accont_image.setImageResource(R.drawable.icn_y);
+        passcode_title.setText(getString(R.string.passcode_title));
+        ((LoggedInActivity) getActivity()).updateTitle(getString(R.string.login));
+    }
+
+
     /**
      * update screen's text as per account pincode's verification
      */
@@ -143,6 +153,7 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
         accont_image.setImageResource(R.drawable.icn_secure);
         passcode_title.setText(getString(R.string.passcode_step2_title));
         passcode_description.setText(getString(R.string.passcode_step2_desc));
+        ((PasscodeActivity) getActivity()).updateTitle(getString(R.string.pincode));
     }
 
     /**
@@ -152,6 +163,7 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
         accont_image.setImageResource(R.drawable.icn_account_created);
         passcode_title.setText(getString(R.string.passcode_step1_title));
         passcode_description.setText(getString(R.string.passcode_step1_desc));
+        ((PasscodeActivity) getActivity()).updateTitle(getString(R.string.pincode));
     }
 
 
@@ -163,35 +175,23 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
                 if (v.getId() == R.id.passcode4) {
                     if (passcode4.getText().length() == 0) {
                         passcode3.requestFocus();
-                        passcode3.setEnabled(true);
-                        passcode3.setSelection(passcode3.getText().length());
-                    }
-                    if (passcode3.getText().length() == 0) {
-                        passcode2.requestFocus();
-                        passcode2.setEnabled(true);
-                        passcode2.setSelection(passcode2.getText().length());
-                    }
-                    if (passcode2.getText().length() == 0) {
-                        passcode1.requestFocus();
-                        passcode1.setEnabled(true);
-                        passcode1.setSelection(passcode1.getText().length());
+                        passcode3.setFocusableInTouchMode(true);
+                        passcode4.setFocusableInTouchMode(false);
+                        passcode3.setText("");
                     }
                 } else if (v.getId() == R.id.passcode3) {
                     if (passcode3.getText().length() == 0) {
                         passcode2.requestFocus();
-                        passcode2.setEnabled(true);
-                        passcode2.setSelection(passcode2.getText().length());
-                    }
-                    if (passcode2.getText().length() == 0) {
-                        passcode1.requestFocus();
-                        passcode1.setEnabled(true);
-                        passcode1.setSelection(passcode1.getText().length());
+                        passcode2.setFocusableInTouchMode(true);
+                        passcode3.setFocusableInTouchMode(false);
+                        passcode2.setText("");
                     }
                 } else if (v.getId() == R.id.passcode2) {
                     if (passcode2.getText().length() == 0) {
                         passcode1.requestFocus();
-                        passcode1.setEnabled(true);
-                        passcode1.setSelection(passcode1.getText().length());
+                        passcode1.setFocusableInTouchMode(true);
+                        passcode2.setFocusableInTouchMode(false);
+                        passcode1.setText("");
                     }
                 }
             }
@@ -204,10 +204,15 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
      */
     protected void resetDigit() {
         passcode1.getText().clear();
+        passcode1.setFocusableInTouchMode(true);
         passcode2.getText().clear();
+        passcode2.setFocusableInTouchMode(false);
         passcode3.getText().clear();
+        passcode3.setFocusableInTouchMode(false);
         passcode4.getText().clear();
+        passcode4.setFocusableInTouchMode(false);
         passcode1.requestFocus();
+
     }
 
     @Override
@@ -215,6 +220,7 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
         switch (eventType) {
             case EventChangeManager.EVENT_PASSCODE_ERROR:
                 passcode_error.setText((String) object);
+                resetDigit();
                 break;
             default:
                 break;
@@ -225,41 +231,36 @@ public class PasscodeFragment extends BaseFragment implements EventChangeListene
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.passcode_reset:
+                //Todo redirect user to reset Pincode process again
                 break;
             default:
                 break;
         }
     }
 
-
     private final class FieldTextWatcher implements TextWatcher {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            if (before == 1 && s.length() == 0) {
-                if (passcode2.hasFocus()) {
-                    passcode1.requestFocus();
-                    passcode1.setEnabled(true);
-                    passcode1.setSelection(passcode1.getText().length());
-                } else if (passcode3.hasFocus()) {
-                    passcode2.requestFocus();
-                    passcode2.setEnabled(true);
-                    passcode2.setSelection(passcode2.getText().length());
-                } else if (passcode4.hasFocus()) {
-                    passcode3.requestFocus();
-                    passcode3.setEnabled(true);
-                    passcode3.setSelection(passcode3.getText().length());
-                }
-            } else if (s.length() == 1) {
+            if (s.length() == 1) {
                 if (passcode1.hasFocus()) {
+                    passcode2.setFocusableInTouchMode(true);
                     passcode2.requestFocus();
-                    passcode1.setEnabled(false);
+                    passcode1.setFocusableInTouchMode(false);
+                   // passcode3.setFocusableInTouchMode(false);
+                   // passcode4.setFocusableInTouchMode(false);
                 } else if (passcode2.hasFocus()) {
+                    //passcode1.setFocusableInTouchMode(false);
+                    passcode3.setFocusableInTouchMode(true);
                     passcode3.requestFocus();
-                    passcode2.setEnabled(false);
+                    passcode2.setFocusableInTouchMode(false);
+                    //passcode4.setFocusableInTouchMode(false);
                 } else if (passcode3.hasFocus()) {
+                    passcode4.setFocusableInTouchMode(true);
                     passcode4.requestFocus();
-                    passcode3.setEnabled(false);
+                    //passcode1.setFocusableInTouchMode(false);
+                    //passcode2.setFocusableInTouchMode(false);
+                    passcode3.setFocusableInTouchMode(false);
                 }
             }
         }
