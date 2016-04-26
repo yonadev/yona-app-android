2e876aaedf456e127db662eafe63df33fcb92a20
Kinnar Vasa <kvasa@mobiquityinc.com>
2016-04-26 17:04:11 +0530

    APPDEV-195
    Show Option of Unregister in Settings
    
    Signed-off-by: Kinnar Vasa <kvasa@mobiquityinc.com>

--- app/src/main/java/nu/yona/app/ui/settings/SettingsFragment.java
+++ app/src/main/java/nu/yona/app/ui/settings/SettingsFragment.java
@@ -9,6 +9,7 @@
 package nu.yona.app.ui.settings;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -19,12 +20,14 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import nu.yona.app.R;
+import nu.yona.app.api.manager.impl.AuthenticateManagerImpl;
 import nu.yona.app.api.manager.impl.DeviceManagerImpl;
 import nu.yona.app.api.model.ErrorMessage;
 import nu.yona.app.customview.CustomAlertDialog;
 import nu.yona.app.customview.YonaFontTextView;
 import nu.yona.app.listener.DataLoadListener;
 import nu.yona.app.ui.BaseFragment;
+import nu.yona.app.ui.LaunchActivity;
 import nu.yona.app.ui.YonaActivity;
 import nu.yona.app.utils.AppConstant;
 import nu.yona.app.utils.AppUtils;
@@ -48,7 +51,7 @@ public class SettingsFragment extends BaseFragment {
 
         activity = (YonaActivity) getActivity();
 
-        listView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.settings_list_item, new String[]{getString(R.string.change_pin), getString(R.string.privacy), getString(R.string.add_device)}));
+        listView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.settings_list_item, new String[]{getString(R.string.change_pin), getString(R.string.privacy), getString(R.string.add_device), getString(R.string.delete_user)}));
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -60,6 +63,8 @@ public class SettingsFragment extends BaseFragment {
                 } else if (((YonaFontTextView) view).getText().toString().equals(getString(R.string.add_device))) {
                     activity.showLoadingView(true, null);
                     addDevice(AppUtils.getRandomString(AppConstant.ADD_DEVICE_PASSWORD_CHAR_LIMIT));
+                } else if(((YonaFontTextView) view).getText().toString().equals(getString(R.string.delete_user))) {
+                    unsubscribeUser();
                 }
             }
         });
@@ -67,6 +72,42 @@ public class SettingsFragment extends BaseFragment {
         return view;
     }
 
+    private void unsubscribeUser() {
+        CustomAlertDialog.show(activity, getString(R.string.delete_user), getString(R.string.delete_user_message),
+                getString(R.string.ok), getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        doUnsubscribe();
+                    }
+                }, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+    }
+
+    private void doUnsubscribe() {
+        activity.showLoadingView(true, null);
+        new AuthenticateManagerImpl(activity).deleteUser(new DataLoadListener() {
+            @Override
+            public void onDataLoad(Object result) {
+                activity.showLoadingView(false, null);
+                startActivity(new Intent(activity, LaunchActivity.class));
+            }
+
+            @Override
+            public void onError(Object errorMessage) {
+                CustomAlertDialog.show(activity, ((ErrorMessage) errorMessage).getMessage(), getString(R.string.ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
+    }
     @Override
     public void onResume() {
         super.onResume();
@@ -93,7 +134,7 @@ public class SettingsFragment extends BaseFragment {
 
     private void showAlert(String message, final boolean doDelete) {
         activity.showLoadingView(false, null);
-        CustomAlertDialog.show(getActivity(),
+        CustomAlertDialog.show(activity,
                 message,
                 getString(R.string.ok), new DialogInterface.OnClickListener() {
                     @Override
