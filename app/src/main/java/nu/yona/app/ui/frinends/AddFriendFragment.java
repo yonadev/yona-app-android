42eaf0cad5286a6ade54aba7642a279f81f27b1e
Kinnar Vasa <kvasa@mobiquityinc.com>
2016-05-03 16:20:47 +0530

    APPDEV-43
    Permission Alert for Marshmallow and above version.
    
    Signed-off-by: Kinnar Vasa <kvasa@mobiquityinc.com>

--- app/src/main/java/nu/yona/app/ui/frinends/AddFriendFragment.java
+++ app/src/main/java/nu/yona/app/ui/frinends/AddFriendFragment.java
@@ -10,9 +10,13 @@
 
 package nu.yona.app.ui.frinends;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +27,7 @@ import nu.yona.app.YonaApplication;
 import nu.yona.app.state.EventChangeManager;
 import nu.yona.app.ui.BaseFragment;
 import nu.yona.app.ui.ViewPagerAdapter;
+import nu.yona.app.ui.YonaActivity;
 
 /**
  * Created by kinnarvasa on 27/04/16.
@@ -31,6 +36,7 @@ public class AddFriendFragment extends BaseFragment {
 
     private final int ADD_FRIEND_MANUALLY = 0, ADD_FRIENT_CONTACT = 1;
     private ViewPager viewPager;
+    private final int READ_CONTACTS_PERMISSIONS_REQUEST = 1;
 
     @Nullable
     @Override
@@ -46,7 +52,7 @@ public class AddFriendFragment extends BaseFragment {
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
         adapter.addFragment(new AddFriendManually(), getString(R.string.addfriendmanually));
-        adapter.addFragment(new OverviewFragment(), getString(R.string.addfriendcontacts));
+        adapter.addFragment(new AddFriendContacts(), getString(R.string.addfriendcontacts));
         viewPager.setAdapter(adapter);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
@@ -55,7 +61,7 @@ public class AddFriendFragment extends BaseFragment {
 
             @Override
             public void onPageSelected(int position) {
-                if (position == ADD_FRIENT_CONTACT) {
+                if (position == ADD_FRIENT_CONTACT && getPermissionToReadUserContacts()) {
                     openContactBook();
                 }
             }
@@ -66,6 +72,15 @@ public class AddFriendFragment extends BaseFragment {
         });
     }
 
+    public boolean getPermissionToReadUserContacts() {
+        ((YonaActivity) getActivity()).setSkipVerification(true);
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, READ_CONTACTS_PERMISSIONS_REQUEST);
+            return false;
+        }
+        return true;
+    }
+
     private void openContactBook() {
         YonaApplication.getEventChangeManager().notifyChange(EventChangeManager.EVENT_OPEN_CONTACT_BOOK, null);
     }
@@ -74,5 +89,20 @@ public class AddFriendFragment extends BaseFragment {
     public void onResume() {
         super.onResume();
         viewPager.setCurrentItem(ADD_FRIEND_MANUALLY, true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        // Make sure it's our original READ_CONTACTS request
+        if (requestCode == READ_CONTACTS_PERMISSIONS_REQUEST) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openContactBook();
+            } else {
+                viewPager.setCurrentItem(ADD_FRIEND_MANUALLY);
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
 }
