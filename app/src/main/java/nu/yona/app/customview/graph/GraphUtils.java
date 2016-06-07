613abe5aea4af24c4dab4c849da6a5cd63869faa
Bhargav <bsuthar@mobiquityinc.com>
2016-06-07 16:02:37 +0530

    Implemeted TimeBucket Control

---
+++ app/src/main/java/nu/yona/app/customview/graph/GraphUtils.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 Stichting Yona Foundation
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package nu.yona.app.customview.graph;
+
+/**
+ * Created by bhargavsuthar on 07/06/16.
+ */
+public class GraphUtils {
+    public static int COLOR_PINK = 0xffe8308a;
+    public static int COLOR_BLUE = 0xff1d71b8;
+    public static int COLOR_GREEN = 0xff8ab518;
+    public static int COLOR_BACKGROUD_GRAPH = 0xfff7f7f7;
+    public static int COLOR_WHITE_THREE = 0xffe7e7e7;
+    public static int COLOR_BULLET_DOT = 0xffd5d5d5;
+    public static int COLOR_BULLET_LIGHT_DOT = 0xfff3f3f3;
+    public static int COLOR_TEXT = 0xff2f2f2f;
+    public static int TEXT_SIZE = 12;
+
+}
