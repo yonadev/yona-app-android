b832c874c7fd61b39bc977b00e19c6e79fa730ea
Bhargav <bsuthar@mobiquityinc.com>
2016-06-29 20:11:03 +0530

    [APPDEV-467] Implement adapter for timeline

---
+++ app/src/main/java/nu/yona/app/ui/frinends/TimelineHolder.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2016 Stichting Yona Foundation
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package nu.yona.app.ui.frinends;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+
+import nu.yona.app.R;
+import nu.yona.app.customview.YonaFontTextView;
+import nu.yona.app.customview.graph.TimeBucketGraph;
+import nu.yona.app.customview.graph.TimeFrameGraph;
+import nu.yona.app.enums.ChartTypeEnum;
+
+/**
+ * Created by bhargavsuthar on 28/06/16.
+ */
+public class TimelineHolder extends RecyclerView.ViewHolder {
+
+    private YonaFontTextView mHeaderCategoryTypeGoal;
+    private YonaFontTextView mHeaderCategoryGoalTime;
+    private ImageView mUserIcon;
+    private TimeBucketGraph mTimebucketGraph;
+    private TimeFrameGraph mTimeFrameGraph;
+    private ImageView mNogoImage;
+    private YonaFontTextView mTxtNogo;
+    private YonaFontTextView mTxtNogoTime;
+    private View view;
+
+    public TimelineHolder(View itemView, View.OnClickListener listener, ChartTypeEnum chartTypeEnum) {
+        super(itemView);
+        this.view = itemView;
+        initHeaderControl();
+        switch (chartTypeEnum) {
+            case NOGO_CONTROL:
+                initNoGOControlView();
+                break;
+            case TIME_BUCKET_CONTROL:
+                initTimeBucketControlView();
+                break;
+            case TIME_FRAME_CONTROL:
+                initTimeFrameControlView();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initHeaderControl() {
+        mHeaderCategoryTypeGoal = (YonaFontTextView) view.findViewById(R.id.txtHeaderSection);
+        mHeaderCategoryGoalTime = (YonaFontTextView) view.findViewById(R.id.txtHeaderMinuten);
+    }
+
+    private void initNoGOControlView() {
+        mTxtNogo = (YonaFontTextView) view.findViewById(R.id.txtNoGoText);
+        mTxtNogoTime = (YonaFontTextView) view.findViewById(R.id.txtNogoTime);
+        mNogoImage = (ImageView) view.findViewById(R.id.img_nogo_user_icon);
+    }
+
+    private void initTimeBucketControlView() {
+        mTimebucketGraph = (TimeBucketGraph) view.findViewById(R.id.timeBucketGraph);
+        mUserIcon = (ImageView) view.findViewById(R.id.img_user_icon);
+
+    }
+
+    private void initTimeFrameControlView() {
+        mTimeFrameGraph = (TimeFrameGraph) view.findViewById(R.id.timeFrameControl);
+        mUserIcon = (ImageView) view.findViewById(R.id.img_user_icon);
+    }
+
+
+    public YonaFontTextView getmHeaderCategoryTypeGoal() {
+        return mHeaderCategoryTypeGoal;
+    }
+
+    public void setmHeaderCategoryTypeGoal(YonaFontTextView mHeaderCategoryTypeGoal) {
+        this.mHeaderCategoryTypeGoal = mHeaderCategoryTypeGoal;
+    }
+
+    public YonaFontTextView getmHeaderCategoryGoalTime() {
+        return mHeaderCategoryGoalTime;
+    }
+
+    public void setmHeaderCategoryGoalTime(YonaFontTextView mHeaderCategoryGoalTime) {
+        this.mHeaderCategoryGoalTime = mHeaderCategoryGoalTime;
+    }
+
+    public ImageView getmUserIcon() {
+        return mUserIcon;
+    }
+
+    public void setmUserIcon(ImageView mUserIcon) {
+        this.mUserIcon = mUserIcon;
+    }
+
+    public TimeBucketGraph getmTimebucketGraph() {
+        return mTimebucketGraph;
+    }
+
+    public void setmTimebucketGraph(TimeBucketGraph mTimebucketGraph) {
+        this.mTimebucketGraph = mTimebucketGraph;
+    }
+
+    public TimeFrameGraph getmTimeFrameGraph() {
+        return mTimeFrameGraph;
+    }
+
+    public void setmTimeFrameGraph(TimeFrameGraph mTimeFrameGraph) {
+        this.mTimeFrameGraph = mTimeFrameGraph;
+    }
+
+    public ImageView getmNogoImage() {
+        return mNogoImage;
+    }
+
+    public void setmNogoImage(ImageView mNogoImage) {
+        this.mNogoImage = mNogoImage;
+    }
+
+    public YonaFontTextView getmTxtNogo() {
+        return mTxtNogo;
+    }
+
+    public void setmTxtNogo(YonaFontTextView mTxtNogo) {
+        this.mTxtNogo = mTxtNogo;
+    }
+
+    public YonaFontTextView getmTxtNogoTime() {
+        return mTxtNogoTime;
+    }
+
+    public void setmTxtNogoTime(YonaFontTextView mTxtNogoTime) {
+        this.mTxtNogoTime = mTxtNogoTime;
+    }
+
+    public View getView() {
+        return view;
+    }
+
+    public void setView(View view) {
+        this.view = view;
+    }
+
+}
---
+++ app/src/main/java/nu/yona/app/ui/frinends/TimelineStickyAdapter.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2016 Stichting Yona Foundation
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package nu.yona.app.ui.frinends;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.timehop.stickyheadersrecyclerview.StickyRecyclerHeadersAdapter;
+
+import java.util.List;
+
+import nu.yona.app.R;
+import nu.yona.app.api.model.DayActivity;
+import nu.yona.app.customview.YonaFontTextView;
+import nu.yona.app.enums.ChartTypeEnum;
+import nu.yona.app.ui.YonaActivity;
+import nu.yona.app.utils.AppConstant;
+
+/**
+ * Created by bhargavsuthar on 28/06/16.
+ */
+public class TimelineStickyAdapter extends RecyclerView.Adapter<TimelineHolder> implements StickyRecyclerHeadersAdapter<RecyclerView.ViewHolder> {
+
+    private List<DayActivity> dayActivityList;
+    private View.OnClickListener listener;
+    private Context mContext;
+
+    /**
+     * Instantiates a new Per day sticky adapter.
+     *
+     * @param chartItem the chart item
+     * @param listener  the listener
+     */
+    public TimelineStickyAdapter(List<DayActivity> chartItem, View.OnClickListener listener) {
+        this.dayActivityList = chartItem;
+        this.listener = listener;
+    }
+
+    @Override
+    public TimelineHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+
+        View childView = null;
+        switch (ChartTypeEnum.getChartTypeEnum(viewType)) {
+            case NOGO_CONTROL:
+                childView = LayoutInflater.from(parent.getContext()).inflate(R.layout.timeline_nogo_layout, parent, false);
+                break;
+            case TIME_BUCKET_CONTROL:
+                childView = LayoutInflater.from(parent.getContext()).inflate(R.layout.timeline_timebucket_layout, parent, false);
+                break;
+            case TIME_FRAME_CONTROL:
+                childView = LayoutInflater.from(parent.getContext()).inflate(R.layout.timeline_timeframe_layout, parent, false);
+                break;
+            case TITLE:
+                childView = LayoutInflater.from(parent.getContext()).inflate(R.layout.header_timeline, parent, false);
+                break;
+            case LINE:
+                childView = LayoutInflater.from(parent.getContext()).inflate(R.layout.horizontal_line_view, parent, false);
+                break;
+            default:
+                break;
+        }
+        return new TimelineHolder(childView, listener, ChartTypeEnum.getChartTypeEnum(viewType));
+    }
+
+    @Override
+    public void onBindViewHolder(TimelineHolder holder, int position) {
+        DayActivity dayActivity = (DayActivity) getItem(position);
+
+        if (dayActivity != null) {
+            holder.getView().setTag(dayActivity);
+            switch (dayActivity.getChartTypeEnum()) {
+                case TIME_FRAME_CONTROL:
+                    if (dayActivity.getTimeZoneSpread() != null) {
+                        holder.getmTimeFrameGraph().chartValuePre(dayActivity.getTimeZoneSpread());
+                    }
+                    updateProfileImage(holder, dayActivity);
+                    break;
+                case TIME_BUCKET_CONTROL:
+                    int maxDurationAllow = (int) dayActivity.getYonaGoal().getMaxDurationMinutes();
+                    if (maxDurationAllow > 0) {
+                        holder.getmTimebucketGraph().graphArguments(dayActivity.getTotalMinutesBeyondGoal(), (int) dayActivity.getYonaGoal().getMaxDurationMinutes(), dayActivity.getTotalActivityDurationMinutes());
+                    }
+                    updateProfileImage(holder, dayActivity);
+                    break;
+                case SPREAD_CONTROL:
+                    break;
+                case NOGO_CONTROL:
+                    if (dayActivity.getGoalAccomplished()) {
+                        holder.getmNogoImage().setImageResource(R.drawable.adult_happy);
+                    } else {
+                        holder.getmNogoImage().setImageResource(R.drawable.adult_sad);
+                    }
+                    holder.getmTxtNogo().setText(dayActivity.getYonaGoal().getNickName());
+                    holder.getmTxtNogoTime().setText(mContext.getString(R.string.nogogoalbeyond, dayActivity.getYonaGoal().getMaxDurationMinutes()));
+                    break;
+                case TITLE:
+                    holder.getmHeaderCategoryTypeGoal().setText(dayActivity.getYonaGoal().getActivityCategoryName());
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void updateProfileImage(TimelineHolder holder, DayActivity dayActivity) {
+        holder.getmUserIcon().setImageDrawable(TextDrawable.builder()
+                .beginConfig().withBorder(AppConstant.PROFILE_ICON_BORDER_SIZE).endConfig()
+                .buildRound(dayActivity.getYonaGoal().getNickName().substring(0, 1).toUpperCase(),
+                        ContextCompat.getColor(YonaActivity.getActivity(), R.color.grape)));
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return dayActivityList.get(position).getChartTypeEnum().getId();
+    }
+
+    /**
+     * Gets item.
+     *
+     * @param position the position
+     * @return the item
+     */
+    public Object getItem(int position) {
+        return dayActivityList.get(position);
+    }
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.message_header_layout, parent, false);
+        return new RecyclerView.ViewHolder(view) {
+        };
+    }
+
+    @Override
+    public long getHeaderId(int position) {
+        return dayActivityList.get(position).getStickyHeaderId();
+    }
+
+    @Override
+    public void onBindHeaderViewHolder(RecyclerView.ViewHolder holder, int position) {
+        YonaFontTextView textView = (YonaFontTextView) holder.itemView;
+        Object yonaObject = getItem(position);
+        if (yonaObject != null) {
+            textView.setText(((DayActivity) yonaObject).getStickyTitle());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return dayActivityList.size();
+    }
+
+    /**
+     * Notify data set change.
+     *
+     * @param dayActivities the yona messages
+     */
+    public void notifyDataSetChange(final List<DayActivity> dayActivities) {
+        this.dayActivityList = dayActivities;
+        notifyDataSetChanged();
+    }
+
+    /**
+     * Clear.
+     */
+    public void clear() {
+        while (getItemCount() > 0) {
+            remove((DayActivity) getItem(0));
+        }
+    }
+
+    /**
+     * Remove.
+     *
+     * @param item the item
+     */
+    public void remove(DayActivity item) {
+        int position = dayActivityList.indexOf(item);
+        if (position > -1) {
+            dayActivityList.remove(position);
+            notifyItemRemoved(position);
+        }
+    }
+}
