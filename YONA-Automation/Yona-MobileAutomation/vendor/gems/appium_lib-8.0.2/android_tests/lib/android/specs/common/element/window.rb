01d7fcc368a5d68087d1ffd5227468f094902df5
Bert Roos <Bert-R@users.noreply.github.com>
2018-08-17 08:55:28 +0200

    APPDEV-1152 Implement fastlane deployment (#504)
    * APPDEV-1152 Initial fastlane commit
    
    * APPDEV-1152 Run build stage in Docker container
    
    * APPDEV-1152 Remove SDK update
    
    This is already part of the container image now.
    
    * Updated versionCode for build 3 
    
    * APPDEV-1152 Add publish as beta
    
    Along with this moved the agent directive up. Later we should use the nested stage feature, but that's not available on our Dev@Cloud system.
    
    * APPDEV-1152 withCredentials GoogleJsonKeyData
    
    * Updated versionCode for build 6 
    
    * APPDEV-1152 Use cd instead of dir step
    
    The dir step doesn't due to https://issues.jenkins-ci.org/browse/JENKINS-33510
    
    * Updated versionCode for build 8 
    
    * APPDEV-1152 Omit bundle exec
    
    * Updated versionCode for build 10 
    
    * APPDEV-1152 Try getting current version codes
    
    * Updated versionCode for build 12 
    
    * APPDEV-1152 Updated fastlane before using it
    
    * Updated versionCode for build 14 
    
    * APPDEV-1152 more debugging
    
    * Updated versionCode for build 16 
    
    * APPDEV-1152 More debugging
    
    * Updated versionCode for build 18 
    
    * APPDEV-1152 print version codes
    
    With help of https://stackoverflow.com/a/51855799/4353482
    
    * Updated versionCode for build 20 
    
    * APPDEV-1152 add --verbose flag
    
    * Updated versionCode for build 22 
    
    * APPDEV-1152 Put upload back to see verbose output
    
    * Updated versionCode for build 24 
    
    * APPDEV-1152 Specified the APK to upload
    
    * Updated versionCode for build 26 
    
    * APPDEV-1152 Added missing comma
    
    * Updated versionCode for build 28 
    
    * APPDEV-1152 Added find
    
    To find location of .apk files
    
    * Updated versionCode for build 30 
    
    * APPDEV-1152 Updated APK path
    
    And removed find
    
    * Updated versionCode for build 32 
    
    * APPDEV-1152 Assemble non-debug too
    
    And print paths again
    
    * APPDEV-1152 Set up code signing
    
    Along with this removed the extra build step assembleDebug, as that's part of the general assemble task
    
    * APPDEV-1152 Just do a single Gradle command
    
    With all tasks combined rather than two separate runs with different tasks
    
    * APPDEV-1152 Correct credential type to file
    
    * Updated versionCode for build 37 
    
    * APPDEV-1152 Upload release APK
    
    * Updated versionCode for build 39 
    
    * APPDEV-1152 Set up for build promotion
    
    From alpha to beta to production.
    Upload is commented out for now, as it fails because we don't have the right keystore yet
    
    * Updated versionCode for build 41 
    
    * APPDEV-1152 Remove superfluous build flavors
    
    Only 2 remain: development and production
    
    * APPDEV-1152 Updated gradle task
    
    From testZacceptanceDebugUnitTest into testDevelopmentDebugUnitTest
    
    * Updated versionCode for build 44 
    
    * APPDEV-1152 Made uploading to Google Play a stage on its own
    
    Along with this removed YONA-Automation.
    
    At the moment, the current branch is treated as master or develop, for testing purposes.
    
    * Updated versionCode for build 46 
    
    * APPDEV-1152 Corrected condition for branches to anyOf
    
    * Updated versionCode for build 48 
    
    * APPDEV-1152 Revert version.properties

--- YONA-Automation/Yona-MobileAutomation/vendor/gems/appium_lib-8.0.2/android_tests/lib/android/specs/common/element/window.rb
+++
@@ -1,9 +0,0 @@
-describe 'common/element/window' do
-  t 'window_size' do
-    wait do
-      size = window_size
-      size.width.class.must_equal Fixnum
-      size.height.class.must_equal Fixnum
-    end
-  end
-end
--- YONA-Automation/Yona-MobileAutomation/vendor/gems/appium_lib-8.0.2/android_tests/lib/android/specs/common/version.rb
+++
@@ -1,9 +0,0 @@
-describe 'version.rb' do
-  t '::Appium::VERSION' do
-    ::Appium::VERSION.must_match(/(\d+)\.(\d+).(\d+)/)
-  end
-
-  t '::Appium::DATE' do
-    ::Appium::DATE.must_match(/(\d+)\-(\d+)\-(\d+)/)
-  end
-end
