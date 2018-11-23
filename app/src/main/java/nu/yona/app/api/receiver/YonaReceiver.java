718f813447948e515324870577435bf7c27e5941
Sivateja-CMI <35359277+Sivateja-CMI@users.noreply.github.com>
2018-11-23 22:01:54 +0530

    APPDEV-1196 App activity monitoring notification disappears. (#529)
    * APPDEV-1196 App activity monitoring notification disappears.
    
    -- Added device_awake alarm broadcast for starting foreground service. This fires only after device goes into doze mode and OS above OREO.
    
    * APPDEV-1196 Removed un-necessary Toast messages.
    
    * APPDEV-1196 Alarms as substitute for SCREEN_ON broadcast.
    
    -- After receiving the SCREEN_OFF broadcast, app schedules an recursive alarm for 10 seconds.Upon firing this alarm, if device is interactive, app starts the service and stops the recursive alarms until next SCREEN_OFF broadcast else it will schedule next alarm.
    
    * APPDEV-1196 Review changes.
    
    -- Removed unnecessary constants and logs.
    -- Language corrections.
    
    * APPDEV-1196 Review changes. Deleted  un-necessary log.

--- app/src/main/java/nu/yona/app/api/receiver/YonaReceiver.java
+++ app/src/main/java/nu/yona/app/api/receiver/YonaReceiver.java
@@ -8,6 +8,8 @@
 
 package nu.yona.app.api.receiver;
 
+import android.annotation.TargetApi;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -16,6 +18,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.os.PowerManager;
 
 import nu.yona.app.R;
 import nu.yona.app.YonaApplication;
@@ -24,44 +27,108 @@ import nu.yona.app.utils.AppConstant;
 import nu.yona.app.utils.AppUtils;
 import nu.yona.app.utils.Logger;
 
+import static android.content.Context.POWER_SERVICE;
+
 /**
  * Created by kinnarvasa on 23/03/16.
  */
 public class YonaReceiver extends BroadcastReceiver
 {
+	private static final int INTERACTIVE_CHECK_INTERVAL = 10000;
 
-	private Context mContext;
+	private Context context;
 
 	@Override
 	public void onReceive(Context context, Intent intent)
 	{
-		this.mContext = context;
+		this.context = context;
 		switch (intent.getAction())
 		{
 			case Intent.ACTION_BOOT_COMPLETED:
-				Logger.loge("ACTION_BOOT_COMPLETED On", "ACTION_BOOT_COMPLETED On");
-				startService(context);
+				handleRebootCompletedBroadcast(context);
 			case Intent.ACTION_SCREEN_ON:
-				Logger.loge("Screen On", "Screen On");
-				startService(context);
-				AppUtils.startVPN(context, false);
+				handleScreenOnBroadcast(context);
 				break;
 			case Intent.ACTION_SCREEN_OFF:
-				Logger.loge("SEND_Screen Off", "Screen Off");
-				AppUtils.setNullScheduler();
-				AppUtils.sendLogToServer(AppConstant.ONE_SECOND);
-				AppUtils.stopService(context);
+				handleScreenOffBroadcast(context);
+				break;
+			case AppConstant.WAKE_UP:
+				handleWakeUpAlarm(context);
 				break;
 			case AppConstant.RESTART_DEVICE:
 				YonaApplication.getEventChangeManager().notifyChange(EventChangeManager.EVENT_DEVICE_RESTART_REQUIRE, null);
 				break;
 			case AppConstant.RESTART_VPN:
-				Logger.loge("Show restart VPN calll", "Show restart vpn call");
-				showRestartVPN(mContext.getString(R.string.vpn_disconnected));
+				handleRestartVPNBroadcast(context);
 				break;
 			default:
 				break;
 		}
+	}
+
+	private void handleRebootCompletedBroadcast(Context context)
+	{
+		Logger.loge("ACTION_BOOT_COMPLETED On", "ACTION_BOOT_COMPLETED On");
+		startService(context);
+	}
+
+	private void handleScreenOnBroadcast(Context context)
+	{
+		Logger.logi("Screen On", "Screen On");
+		startService(context);
+		AppUtils.startVPN(context, false);
+	}
+
+	private void handleScreenOffBroadcast(Context context)
+	{
+		Logger.logi("SEND_Screen Off", "Screen Off");
+		AppUtils.setNullScheduler();
+		AppUtils.sendLogToServer(AppConstant.ONE_SECOND);
+		AppUtils.stopService(context);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+		{
+			scheduleNextAlarmToCheckIfDeviceIsInteractive(context, INTERACTIVE_CHECK_INTERVAL);
+		}
+	}
+
+	@TargetApi(Build.VERSION_CODES.O)
+	private void handleWakeUpAlarm(Context context)
+	{
+		// Device is awake from doze/sleep (it can be because of user interaction or of some silent Push notifications).
+		// We should start service only when device is interactive else schedule next alarm
+		if (isDeviceInteractive(context))
+		{
+			startService(context);
+			AppUtils.startVPN(context, false);
+		}
+		else
+		{
+			scheduleNextAlarmToCheckIfDeviceIsInteractive(context, INTERACTIVE_CHECK_INTERVAL);
+		}
+	}
+
+	@TargetApi(Build.VERSION_CODES.O)
+	private boolean isDeviceInteractive(Context context)
+	{
+		PowerManager powerManager = (PowerManager) context.getSystemService(POWER_SERVICE);
+		return powerManager.isInteractive();
+	}
+
+	@TargetApi(Build.VERSION_CODES.O)
+	public static void scheduleNextAlarmToCheckIfDeviceIsInteractive(Context context, long delay)
+	{
+		Intent alarmIntent = new Intent(context, YonaReceiver.class);
+		alarmIntent.setAction(AppConstant.WAKE_UP);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.cancel(pendingIntent);
+		alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + delay, pendingIntent);
+	}
+
+	private void handleRestartVPNBroadcast(Context context)
+	{
+		Logger.logi("VPN", "Restart VPN Broadcast received");
+		showRestartVPN(context.getString(R.string.vpn_disconnected));
 	}
 
 	private void startService(Context context)
@@ -73,29 +140,30 @@ public class YonaReceiver extends BroadcastReceiver
 		}
 	}
 
+
 	private void showRestartVPN(final String message)
 	{
 		try
 		{
-			Intent intent = AppUtils.startVPN(mContext, true);
-			PendingIntent pIntent = PendingIntent.getActivity(mContext, (int) System.currentTimeMillis() + 10000, intent, 0);
+			Intent intent = AppUtils.startVPN(context, true);
+			PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis() + 10000, intent, 0);
 
-			Notification notification = new Notification.Builder(mContext).setContentTitle(mContext.getString(R.string.appname))
+			Notification notification = new Notification.Builder(context).setContentTitle(context.getString(R.string.appname))
 					.setContentText(message)
-					.setTicker(mContext.getString(R.string.appname))
+					.setTicker(context.getString(R.string.appname))
 					.setWhen(0)
 					.setVibrate(new long[]{1, 1, 1})
 					.setDefaults(Notification.DEFAULT_SOUND)
 					.setStyle(new Notification.BigTextStyle().bigText(message))
 					.setSmallIcon(R.mipmap.ic_launcher)
-					.setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), R.mipmap.ic_launcher))
+					.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher))
 					.setContentIntent(pIntent)
 					.setAutoCancel(true)
 					.build();
 
 			notification.flags |= Notification.FLAG_NO_CLEAR;
 
-			NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(mContext.NOTIFICATION_SERVICE);
+			NotificationManager notificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
 			notificationManager.notify(0, notification);
 		}
 		catch (Exception e)
